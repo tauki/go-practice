a9e0e73989d6d852b752b667ec75f52077c70eb8
Tauki Tahmid <ttauki@gmail.com>
2018-06-28 15:41:27 +0600

    add template handler and config

--- go-programming-blueprint/ch01/chat/main.go
+++ go-programming-blueprint/ch01/chat/main.go
@@ -1,23 +1,33 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
- 			<html>
-				<head>
- 					<title>Chat</title>
- 				</head>
- 				<body>
- 					Let's chat!
- 				</body>
- 			</html>
- 		`))
+// templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request.
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates",
+			t.filename)))
 	})
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+
 	// start the web server
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
