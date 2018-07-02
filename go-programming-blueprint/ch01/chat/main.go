b04f0de8e11eb719152e6bb7c62b073b10079f92
Tauki Tahmid <ttauki@gmail.com>
2018-07-02 18:01:32 +0600

    Add flag for the port address

--- go-programming-blueprint/ch01/chat/main.go
+++ go-programming-blueprint/ch01/chat/main.go
@@ -6,6 +6,7 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+	"flag"
 )
 
 // templ represents a single template
@@ -25,11 +26,16 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-
+	var addr = flag.String("addr", ":8080", "The addr of the application.")
+	flag.Parse() // parse the flags
+	r := newRoom()
 	http.Handle("/", &templateHandler{filename: "chat.html"})
-
+	http.Handle("/room", r)
+	// get the room going
+	go r.run()
 	// start the web server
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
